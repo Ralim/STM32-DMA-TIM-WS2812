f4928a2987a3d3b6ad79a16b4c558c15c7257144
Ben V. Brown <Ralim@Ralimtek.com>
2021-06-30 19:18:25 +1000

    Generate

---
+++ Workspace/DMAWS2812/Core/Driver/WS2812Timer.cpp
@@ -0,0 +1,9 @@
+/*
+ * WS2812Timer.cpp
+ *
+ *  Created on: 30 Jun 2021
+ *      Author: Ralim
+ */
+
+#include "WS2812Timer.h"
+
---
+++ Workspace/DMAWS2812/Core/Driver/WS2812Timer.h
@@ -0,0 +1,14 @@
+/*
+ * WS2812Timer.h
+ *
+ *  Created on: 30 Jun 2021
+ *      Author: Ralim
+ */
+
+#ifndef DRIVER_WS2812TIMER_H_
+#define DRIVER_WS2812TIMER_H_
+
+class WS2812Timer {
+};
+
+#endif /* DRIVER_WS2812TIMER_H_ */
